02903af07a940507712a1655aa12a35d03f73a98
lukasz mocarski <lukasz.mocarski@gmail.com>
2014-11-28 22:04:25 +0100

    Przeniesiona logika do klasy Gra oraz zmiany widoku

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import model.Gracz;
@@ -12,64 +14,97 @@ public class App {
 	}
 
 	public static void pokazUczestnikowWyscigu(Gra gra) {
-		for (Kon k : gra.getUczestnicyWyscigu()) {
-			System.out.println("Kon " + k.getNazwa() + "\tWytrzymalosc: " + k.getWytrzymalosc() + "\tSzybkosc: " + k.getSzybkosc() + "\tSzczescie: " + k.getSzczescie() + "\tWiek: " + k.getWiek());
+		
+		for (int i=0; i<gra.getUczestnicyWyscigu().size(); i++) {
+			
+			Kon k = gra.getUczestnicyWyscigu().get(i);
+			System.out.println("-------------------------------------------------------------------------------------------------");
+			System.out.println((i+1) + ". Kon " + k.getNazwa() + "\tWytrzymalosc: " + k.getWytrzymalosc() +
+					"\tSzybkosc: " + k.getSzybkosc() + "\tSzczescie: " + k.getSzczescie() + "\tWiek: " + k.getWiek());
+		
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		Gra nowaGra = new Gra(1);
 		
-		//while(nowaGra.getListaGraczy().get(0).getPunkty() > 0) {
-		while(nowaGra.CzyTrwa()) {
-			
-			nowaGra.nowyWyscig(100, 5);
-			
-			pokazUczestnikowWyscigu(nowaGra);
-			
-			System.out.println("Twoja gotowka: " + nowaGra.getAktualnyGracz().getPunkty());
-			
-			System.out.println("Stawiam na konia nr: ");
-			int numerKonia = scan.nextInt();
-			nowaGra.getAktualnyGracz().setObstawionyKon(nowaGra.getAktualnyWyscig().ListaKoni().get(numerKonia - 1));
-			
-			System.out.println("Stawiam gotówki: ");
-			double stawka = scan.nextDouble();
-			
-			while (!nowaGra.getAktualnyWyscig().CzyZakonczylSie()) {
-				nowaGra.getAktualnyWyscig().NastepnyRuch();
-				
-				
-				for (Kon k : nowaGra.getAktualnyWyscig().ListaKoni()) {
-					System.out.println(k.getNazwa() + " " + k.getPozycja());
-				}
-				
-				try {
-		            Thread.sleep(20);
-		        } 
-		        catch (InterruptedException e) {
-		        }
-			}
-	
-			System.out.println("W wysciugu udział brali:");
-			pokazUczestnikowWyscigu(nowaGra);
-			
-			System.out.println("Wyscig wygrywa kon: " + nowaGra.getAktualnyWyscig().getZwyciezcaWyscigu().getNazwa());
-			
-			if(nowaGra.getAktualnyWyscig().getZwyciezcaWyscigu() == nowaGra.getAktualnyGracz().getObstawionyKon()) {
-				System.out.println("Gratulacje wygrałeś: " + stawka*3 + "\n");
-				nowaGra.getAktualnyGracz().dodajPunkty(stawka*3);
-			}
-			else {
-				System.out.println("Niestety przegrałeś: " + stawka + "\n");
-				nowaGra.getAktualnyGracz().odejmijPunkty(stawka);
-	
-			}
+		System.out.print("Wpisz liczbę graczy (1-4): ");
+		int liczbaGraczy = scan.nextInt();
+		
+		List<String> imionaGraczy = new ArrayList<String>();
+		for (int i=0; i<liczbaGraczy; i++) {
+			System.out.print("Wpisz imię dla gracza numer " + (i+1) + ": ");
+			imionaGraczy.add(scan.next());
 		}
 		
-		System.out.println("Niestety skonczyla Ci sie gotowka! :( Zagraj jeszcze raz!");
+		Gra nowaGra = new Gra(liczbaGraczy, imionaGraczy);
+		
+		while(nowaGra.CzyTrwa()) {
+			
+			nowaGra.nowyWyscig(100, 10); //TODO z tym też coś ogarnąć!
+			
+			System.out.print("\n\nW wyścigu udział biorą:\n");
+			pokazUczestnikowWyscigu(nowaGra);
+			
+			for (Gracz gracz : nowaGra.getListaGraczy()) {
+				
+				if(gracz.getPunkty() > 0) {
+					
+					System.out.println("\n--------------------------");
+					System.out.println("Gracz: " + gracz.getNazwa());
+					System.out.println("Twoja gotowka: " + gracz.getPunkty());
+					
+					
+					System.out.print("Stawiam gotówki (wpisz 0 jeżeli nie chcesz grać w tej rundzie): ");
+					double stawka = scan.nextDouble();
+					
+					if(stawka != 0) {
+						gracz.setStawka(stawka);
+						System.out.print("Stawiam na konia nr: ");
+						gracz.setObstawionyKon(nowaGra.getAktualnyWyscig().ListaKoni().get(scan.nextInt() - 1));
+					}
+					else {
+						System.out.print("Nie bierzesz udziału w tej rundzie...");
+					}
+				}
+				
+			}
+			
+			nowaGra.startWyscigu();
+	
+			System.out.println("\nW wysciugu udział brały następujące konie:");
+			pokazUczestnikowWyscigu(nowaGra);
+			
+			System.out.println("\nWyscig wygrywa kon: " + nowaGra.getAktualnyWyscig().getZwyciezcaWyscigu().getNazwa());
+			
+			for (Gracz gracz : nowaGra.getListaGraczy()) {
+				
+				if(gracz.getStawka() != 0) {
+					if(nowaGra.getAktualnyWyscig().getZwyciezcaWyscigu() == gracz.getObstawionyKon()) {
+						System.out.print(gracz.getNazwa() + " stawiałeś na: " + gracz.getObstawionyKon().getNazwa() +
+								"! Gratulacje wygrałeś: " + gracz.getStawka() * nowaGra.getMnoznikStawki() + "\n");
+					}
+					else {
+						
+						System.out.print(gracz.getNazwa() + " stawiałeś na: " + gracz.getObstawionyKon().getNazwa() +
+								" i niestety przegrałeś: " + gracz.getStawka() + "\n");
+						
+						if(gracz.getPunkty() <= 0)
+							System.out.println("Niestety skończyła Ci się gotówka... Odpadasz...");
+			
+					}
+				}
+				else
+					System.out.print(gracz.getNazwa() + " nie brałeś udziału w obstawianiu tego wyścigu.");
+				
+			}
+			
+			
+		}
+		
+		System.out.println("Niestety wszystkim graczom skonczyla sie gotowka! :( Game over! Do książek!");
 		
 	}
 
