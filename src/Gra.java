b9f19730ee95e4d9e7cd6cfbb30af57bad5c87e6
lukasz mocarski <lukasz.mocarski@gmail.com>
2014-11-28 22:04:44 +0100

    Przeniesienie logiki z widoku (app)

--- src/Gra.java
+++ src/Gra.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +13,19 @@ import model.Kon;
 import model.Wyscig;
 
 public class Gra {
-
+	
+	private double mnoznikStawki = 3;
 	private List<Gracz> listaGraczy;
 	private Wyscig aktualnyWyscig;
 	private Kon obstawionyKon;
 	private boolean czyTrwa;
 	private Gracz aktualnyGracz;
 	
-	public Gra(int iloscGraczy) {
+	public Gra(int iloscGraczy, List<String> imionaGraczy) {
 		this.listaGraczy = new ArrayList<Gracz>();
 		
 		for(int i=0; i<iloscGraczy; i++)
-			this.listaGraczy.add(new Gracz("imie")); //TODO zrobic to tak zeby w konstruktorze byla przekazywana moze tablica z imionami graczy?!
+			this.listaGraczy.add(new Gracz(imionaGraczy.get(i)));
 		
 		this.aktualnyGracz = this.listaGraczy.get(0);
 				
@@ -50,9 +52,18 @@ public class Gra {
 		return this.aktualnyGracz;
 	}
 	
+	public double getMnoznikStawki() {
+		return this.mnoznikStawki;
+	}
+	
+	public void setMnoznikStawki(double m) {
+		this.mnoznikStawki = m;
+	}
+	
 	public void nowyWyscig(double dystans, int liczbaKoni) {
 		this.aktualnyWyscig =  new Wyscig(dystans, liczbaKoni);
 	}
+	
 	public List<Kon> getUczestnicyWyscigu() {
 		return this.aktualnyWyscig.ListaKoni();
 	}
@@ -67,4 +78,52 @@ public class Gra {
 			this.aktualnyGracz = this.listaGraczy.get(0);
 	}
 	
+	private boolean sprawdzCzyKoniec() {
+		for (Gracz gracz : this.listaGraczy) {
+			if (gracz.getPunkty() > 0)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	
+	public void startWyscigu() {
+		
+		int step = 1;
+		while(this.aktualnyWyscig.CzyTrwa()) {
+			this.aktualnyWyscig.NastepnyRuch();
+			
+			System.out.println("---------- " + step + " --------------");
+			for (int i=0; i<this.aktualnyWyscig.ListaKoni().size(); i++) {
+				Kon k = this.aktualnyWyscig.ListaKoni().get(i);
+				System.out.println((i+1) + ". " + k.getNazwa() + " " + k.getPozycja()); //jedyne co bedzie drukowane do konsoli w okienkowej appce
+				
+			}
+			
+			step++;
+		
+			try {
+	            Thread.sleep(20);
+	        } 
+	        catch (InterruptedException e) {
+	        }
+		}
+		
+		for (Gracz gracz : this.listaGraczy) {
+			
+			if(gracz.getStawka() > 0) {
+				
+				if(this.aktualnyWyscig.getZwyciezcaWyscigu() == gracz.getObstawionyKon())
+					gracz.dodajPunkty(gracz.getStawka() * mnoznikStawki);
+				else
+					gracz.odejmijPunkty(gracz.getStawka());
+				
+			}
+		}
+		
+		if(sprawdzCzyKoniec())
+			this.czyTrwa = false;
+		
+	}
 }
