80864fbac69ca8efb7c7e83a04275a34d6420c71
lukasz <lukasz@lukasz-T410-ubuntu>
2014-12-11 18:11:16 +0100

    wyrzuciłem setPoints()

--- src/model/Player.java
+++ src/model/Player.java
@@ -19,16 +19,7 @@ public class Player {
 		return this.name;
 	}
 	
-	
-	//TODO ta klasa z czasem do usuniecia po tym jak zostaną wykorzystane w pełni 
-	//metody odejmijPunkty i dodajPunkty.
-	public void setPoints(double p) {
-		this.points = p;
-	}
-	
-	
-	//TODO dwie klasy, dodaj i odejmij punkty. chyba tu przydalaby sie jakas walidacja co sprawdzalaby
-	// czy punktow aby nie jest <0
+
 	public void subtractPoints(double p) {
 		this.points -= p;
 	}
